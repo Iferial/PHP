19275b031b42ea3c0e5a5af7dc6f2983a8b40516
Ivan Samoylenko <santissen@gmail.com>
2019-03-05 21:03:31 +0200

    Third lesson

---
+++ JS.less4.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var num1, num2, result;
+
+num1 = +prompt('Введите первое число',0);
+num2 = +prompt('Введите второе число',0);
+
+ result = num1 + num2;
+
+ alert('Результат сложения: ' + result);
+
