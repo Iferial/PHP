32a47e5148d074aa444fec7bc5e4c7704f99a15e
Iferial <santissen@gmail.com>
2019-03-27 13:27:04 +0200

    Less8

--- Task1.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var num1 = prompt("Каково настоящее нахвание JavaScript?");
-
-if (num1 === 'ECMAScript') {
-    alert('Верно');
-} else {
-    alert('Не знаете? "ECMAScript"!');
-}
